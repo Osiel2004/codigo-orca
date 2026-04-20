abad1b5f21ba455841369e376a262b747e2f5f89
Osiel2004 <al22020345@itsa.edu.mx>
2026-04-20 11:30:19 -0600

    Initial commit

---
+++ CodigoOrca/Scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+# Corrección: @onready todo junto
+@onready var panel_creditos = $PanelCreditos
+
+# 1. FUNCIÓN PARA EL BOTÓN JUGAR
+func _on_boton_jugar_pressed():
+	print("Cargando el mundo de Pánfilo...")
+
+# 2. FUNCIÓN PARA EL BOTÓN CRÉDITOS
+func _on_boton_creditos_pressed():
+	panel_creditos.visible = true
+
+# 3. FUNCIÓN PARA EL BOTÓN VOLVER (Dentro de Créditos)
+func _on_boton_volver_pressed():
+	panel_creditos.visible = false
+
+# 4. FUNCIÓN PARA EL BOTÓN SALIR
+func _on_boton_salir_pressed():
+	print("Saliendo del juego...")
+	get_tree().quit()
